dac261fe8506fa91d6d46ccec89471a05f98d2fd
Sudhanshu Yadav <sudhanshuy748@gmail.com>
2023-08-30 13:07:27 +0530

    Initial commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./footer.module.css";
+import Image from "next/image";
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <div>@Sudhanshu</div>
+      <div className={styles.social}>
+        <Image src="/1.png" width={15} height={15} className={styles.icon} alt="Facebook Account" />
+        <Image src="/2.png" width={15} height={15} className={styles.icon} alt="Lama Dev" />
+        <Image src="/3.png" width={15} height={15} className={styles.icon} alt="Lama Dev" />
+        <Image src="/4.png" width={15} height={15} className={styles.icon} alt="Lama Dev" />
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
