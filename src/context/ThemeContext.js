e1367f1e35c58b466459642267db642c37daecbe
Sudhanshu Yadav <sudhanshuy748@gmail.com>
2024-04-21 17:04:39 +0530

    added new img

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -13,7 +13,9 @@ export const ThemeProvider = ({ children }) => {
 
   return (
     <ThemeContext.Provider value={{ toggle, mode }}>
-      <div className={`theme ${mode}`}>{children}</div>
+      <div className={`theme ${mode}`}>
+      {children}
+      </div>
     </ThemeContext.Provider>
   );
 };
